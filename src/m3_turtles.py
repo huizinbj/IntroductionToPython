a766b8b19e1a5d07d4a5e02e54f282c8798cf6b2
huizinbj <huizinbj@users.noreply.github.com>
2017-11-27 16:31:15 -0500

    Finished m3, Made my own turtle

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -63,6 +63,7 @@ dave = rg.SimpleTurtle()
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
+dave.forward(90)
 
 # ----------------------------------------------------------------------
 # Construct a new turtle and ask it to do things.
@@ -77,7 +78,7 @@ matt.forward(100)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +92,7 @@ matt.forward(100)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +110,15 @@ matt.forward(100)
 #   As always, test by running the module.
 #
 ########################################################################
+Brett = rg.SimpleTurtle("turtle")
+matt.pen = rg.Pen('blue', 50)
+Brett.speed = 5
 
+Brett.right(100)
+Brett.forward(100)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
